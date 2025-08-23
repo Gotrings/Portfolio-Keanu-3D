e54cf4d0e97c1f89a8d8437ab4f8ae3351d34697
Gotrings <keanudustin10@gmail.com>
2025-08-23 11:14:21 +0700

    Update Config 3.6

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import type { Plugin } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/' : './',
   server: {
     host: "::",
     port: 8080,
